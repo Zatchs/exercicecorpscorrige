d42988b49fea1b31f639ebc7a9d9003ee0471932
Zatchs <j.martin@simplon-charleville.fr>
2019-03-28 09:47:16 +0100

    corrigé

---
+++ corps humain (exercice corrigé)/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js%22%3E%3E%22%3E"></script>
+      <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+      <link href="index.css" rel="stylesheet">
+      <title>Le corps humain</title>
+    </head>
+
+    <body>
+      <main>
+        <div id="epaule" class="cercle"></div>
+        <div id="coude" class="cercle"></div>
+        <div id="hanche" class="cercle"></div>
+        <div id="genou" class="cercle"></div>
+
+      <div id="corps"></div>
+
+      <div id="placeholder">
+          <div id="desc-epaule" class="desc">
+              <p>Epaule :</p>
+              <br>
+              <br>
+              <p>La région morphologique de l'épaule (nom féminin) permet la jonction du tronc avec le membre supérieur au niveau du bras. Elle comporte plusieurs articulations la plus mobile du corps humain. Elle permet d'orienter le member supérieur dans l'espace permettant en particulier à son extrémité effectrice, la main, d'assurer ses rôles de préhension et de communication avec l'environnement situé à sa portée.</p>
+          </div>
+          <div id="desc-coude" class="desc">
+              <p>Coude :</p>
+              <br>
+              <p>l'articulation principale des bras, permettant de les plier pour se nourrir par exemple</p>
+          </div>
+          <div id="desc-hanche" class="desc">
+              <p>Hanche: </p>
+              <br>
+              <p>Partie du corps que l'on utilise pour de nombreuses activité nécessitant de bouger comme danser, d'ou l'expression "se déhancher" qui ne veut pas dire se retirer les hanches</p>
+          </div>
+          <div id="desc-genou" class="desc">
+              <p>Genou:</p>
+              <br>
+              <p>Partie du corps servant à gérer les mouvements de jambes dont la marche, on raconte qu'un aventurier s'étant pris une flèche dans le genou a du mettre fin à sa carrière et se reconvertir en garde de ville</p>
+          </div>
+
+      </div>
+      </main>
